f57c935ff2e1f01e5d3a00a5083cecaa45177f3e
Brcoram <89480591+Brcoram@users.noreply.github.com>
2021-12-10 20:34:37 -0500

    Add files via upload

--- final/nav.js
+++ final/nav.js
@@ -10,3 +10,31 @@ function closeMenu() {
     document.querySelector(".hamburger").classList.remove("active");
     document.querySelector(".nav-menu").classList.remove("active");
 }
+
+var modal = document.getElementById("modal");
+var btn = document.getElementsByClassName("modalOpen");
+var span = document.getElementsByClassName("close")[0];
+
+btn[0].onclick = function() {
+  modal.style.display = "block";
+};
+btn[1].onclick = function() {
+  modal.style.display = "block";
+};
+btn[2].onclick = function() {
+  modal.style.display = "block";
+};
+btn[3].onclick = function() {
+  modal.style.display = "block";
+};
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
